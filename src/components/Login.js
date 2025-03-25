3b1a05f8bcfcbc430c5c135a805cb4129d0c0b3f
ducanh005 <vuducanh039@gmail.com>
2025-03-25 21:02:14 +0700

    complete Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { loginApi } from "../services/UserService";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const navigate = useNavigate()
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [isShowPassword, setIsShowPassword] = useState(true);
+
+    const [loadingAPI, setLoadingAPI] = useState(false);
+
+    useEffect(()=>{
+        let token = localStorage.getItem("token");
+        if(token){
+            navigate("/")
+        }
+    },[])
+
+    const handleLogin = async () => {
+        if (!email || !password) {
+            toast.error("Email or Password is equired!");
+            return;
+        }
+        setLoadingAPI(true);
+        let res = await loginApi(email, password);
+        if (res && res.token) {
+            localStorage.setItem("token", res.token);
+            navigate("/")
+        } else {
+            if (res && res.status === 400) {
+                toast.error(res.data.error);
+            }
+        }
+        setLoadingAPI(false);
+    };
     return (
         <div className="login-container col-12 col-sm-4">
             <div className="title ">Login</div>
-            <div className="text">Email or Username</div>
+            <div className="text">Email or Username (eve.holt@reqres.in)</div>
             <input
                 type="text"
                 placeholder="Email or username"
@@ -16,26 +47,31 @@ const Login = () => {
             ></input>
             <div className="input-2">
                 <input
-                    type={isShowPassword === true ?"text" : "password"}
+                    type={isShowPassword === true ? "text" : "password"}
                     placeholder="Password..."
                     value={password}
                     onChange={(event) => setPassword(event.target.value)}
                 ></input>
                 <i
-
-                    className={isShowPassword ===true? "fa-solid fa-eye-slash" :"fa-solid fa-eye"}
+                    className={
+                        isShowPassword === true
+                            ? "fa-solid fa-eye-slash"
+                            : "fa-solid fa-eye"
+                    }
                     onClick={() => setIsShowPassword(!isShowPassword)}
                 ></i>
             </div>
             <button
                 className={email && password ? "active" : ""}
-                disabled={email && password ? false : true}
+                disabled={email && password  ? false : true}
+                onClick={() => handleLogin()}
             >
-                Login
+                {loadingAPI && <i className="fa-solid fa-sync fa-spin"></i>}{" "}
+                &nbsp;Login
             </button>
             <div className="back">
                 {" "}
-                <i class="fa-solid fa-arrow-left"></i> Go back
+                <i className="fa-solid fa-arrow-left"></i> Go back
             </div>
         </div>
     );
