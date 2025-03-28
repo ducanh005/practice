5f217a2d0727fbe62f0a0c269960ec8b2b3766d1
ducanh005 <vuducanh039@gmail.com>
2025-03-28 23:26:24 +0700

    private routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import "./App.scss";
 import Header from "./components/Header";
-import TableUsers from "./components/TableUsers";
 import Container from "react-bootstrap/Container";
 import { ToastContainer } from "react-toastify";
-import Home from "./components/Home";
-import { Route, Routes, Link } from "react-router-dom";
-import Login from "./components/Login";
+
 import { useContext, useEffect } from "react";
 import { UserContext } from "./context/UserContext";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
+    const { user, loginContext } = useContext(UserContext);
 
-    const {user,loginContext}= useContext(UserContext)
-
-    useEffect(()=>{
-        if(localStorage.getItem("token")){
-            loginContext(localStorage.getItem("email"),localStorage.getItem("token"))
+    useEffect(() => {
+        if (localStorage.getItem("token")) {
+            loginContext(
+                localStorage.getItem("email"),
+                localStorage.getItem("token")
+            );
         }
-    },[])
+    }, []);
     return (
         <>
             <div className="app-container">
                 <Header></Header>
                 <Container>
-                <Routes>
-                    <Route path="/" element={<Home/>}></Route>
-                    <Route path="/users" element={<TableUsers/>}></Route>
-                    <Route path="/login" element={<Login/>}></Route>
-                </Routes>
+                    <AppRoutes></AppRoutes>
                 </Container>
             </div>
 
